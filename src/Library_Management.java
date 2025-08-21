78b8f05ceda21edeb0253d169f6b17e8f3686d85
Your Name <Kamalsharma4769@gmail.com>
2025-08-22 01:10:16 +0530

    enhance the implementing code in Library_Management

--- src/Library_Management.java
+++ src/Library_Management.java
@@ -1,32 +1,47 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.*;
+package Jdbc;
 
-import static java.lang.Class.forName;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Library_Management {
-    public static void main(String[] args) throws Exception {
-        Connection con = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        String Url = "jdbc:mysql://localhost:3306/University";
-        String Username = "root";
-        String Password = "Kamal@9090";
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/";
+        String username = "root";
+        String password = "mysql@Ch14";
+        String driverclass = "com.mysql.cj.jdbc.Driver";
+
+        Connection connection = null ;
+        Statement statement = null ;
+        ResultSet rSet = null ;
+
         try {
-            class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(Url,Username,Password);
-            stmt = con.createStatement();
-            String  query = "create database Library_management;";
-            String query2 = "use Library_management;";
-            String query3 = "create table Books(id INT AUTO_INCREMENT PRIMARY KEY,firstname VARCHAR(50) NOT NULL,lastname VARCHAR(100))";
-            String query4 = "insert into Books(firstname , lastname) values('pogo','hello')";
-            rs = stmt.executeQuery(query);
-        } catch{
+            Class.forName(driverclass);
+            connection = DriverManager.getConnection(url , username , password) ;
+            statement = connection.createStatement() ;
+            statement.execute("create database University ;");
+            statement.execute("use University ;") ;
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY,firstname VARCHAR(50) NOT NULL,lastname VARCHAR(100))") ;
+            statement.executeUpdate("insert into students(firstname , lastname) values ('john' , 'cena')") ;
 
+            rSet = statement.executeQuery("select * from students") ;
+            String firstname ;
+            String lastname ;
+            while(rSet.next()) {
+                firstname = rSet.getString("firstname") ;
+                lastname = rSet.getString("lastname");
+                System.out.println(firstname +" " + lastname) ;
+            }
 
-
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception : " + e);
         }
-
+        connection.close();
+        statement.close();
+        rSet.close();
     }
+
 }
